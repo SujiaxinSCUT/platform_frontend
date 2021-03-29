ce724918644ace59988cd1307276d3dc39b57a37
sujiaxin <731744768@qq.com>
2021-03-29 18:32:46 +0800

    add submitting product page

--- src/components/business/SubmitProduct.vue
+++ src/components/business/SubmitProduct.vue
@@ -1,24 +1,80 @@
 <template>
-    <el-dialog id="SubmitProduct" :visible.sync="visible">
-
+    <el-dialog id="SubmitProduct" :visible.sync="dialogVisible" style="" title="提交新产品" :before-close="close"
+        width="450px" :center="true">
+        <el-form :model="form" label-position="left" :rules="rules" ref="form">
+            <el-form-item label="产品名称" :label-width="formLabelWidth" prop="productName" required>
+                <el-input v-model="form.productName" style="width: 200px" size="medium"></el-input>
+            </el-form-item>
+            <el-form-item label="单位" :label-width="formLabelWidth" prop="unit" required>
+                <el-input v-model="form.unit" style="width: 50px;" size="medium"></el-input>
+            </el-form-item>
+            <el-form-item label="产品描述" :label-width="formLabelWidth" prop="description" required>
+                <el-input v-model="form.description" type="textarea" style="width: 200px"></el-input>
+            </el-form-item>
+            <el-form-item label="上传图片" :label-width="formLabelWidth" prop="imageUrl" required style="width: 320px">
+                <el-upload ref="uploadImg" :limit="3" :show-file-list="true"
+                           :before-upload="beforeUpload" action="action"
+                           accept="image/png,image/gif,image/jpg">
+                    <el-button slot="trigger" size="small" type="primary">选取图片</el-button>
+                    <div slot="tip" class="el-upload__tip" style="margin-top: -10px">
+                        只能上传3张以内jpg/png文件，且不超过1MB</div>
+                </el-upload>
+            </el-form-item>
+        </el-form>
+        <div slot="footer">
+            <el-button type="primary" @click="submitForm('form')">提交</el-button>
+        </div>
     </el-dialog>
 </template>
 
 <script>
 export default {
     name: "SubmitProduct",
-    props: ['visible'],
+    props: ['dialogVisible'],
     data() {
         return {
             form: {
-                name: '',
+                productName: '',
+                unit: '',
+                description: '',
+                imageUrl: ''
+            },
+            rules: {
+                productName: [{required: true, trigger: 'blur', message: '请输入产品名称'}],
+                unit: [{required: true, trigger: 'blur', message: '请输入单位'}],
+                description: [{required: true, trigger: 'blur', message: '请输入产品描述'}],
+                imageUrl: [{required: true, trigger: 'blur', message: '请提交产品图片'}],
+            },
 
-            }
+            formLabelWidth: '80px'
+        }
+    },
+    methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            })
+        },
+        close() {
+            this.$emit('close-dialog')
+        },
+        beforeUpload(file) {
+            console.log(file)
+            return true
         }
     }
 }
 </script>
 
 <style scoped>
-
+.el-form {
+    width: 200px;
+    margin-left: 10px;
+    margin-top: -10px;
+}
 </style>
--- src/views/business/MyProduct.vue
+++ src/views/business/MyProduct.vue
@@ -2,7 +2,10 @@
     <div id="MyProduct">
         <el-container>
             <el-header style="text-align: left; height: 30px; margin: 0">
-                <el-button type="primary" style="margin-right: 20px">添加产品</el-button>
+                <el-button type="primary" style="margin-right: 20px"
+                    @click="submitProductDialogVisible = true">
+                    添加产品
+                </el-button>
                 <el-button type="primary">提交新产品</el-button>
             </el-header>
             <el-divider></el-divider>
@@ -10,12 +13,27 @@
 
             </el-main>
         </el-container>
+
+        <submit-product :dialog-visible="submitProductDialogVisible" @close-dialog="closeSubmitProductDialog"/>
     </div>
 </template>
 
 <script>
+import SubmitProduct from "@/components/business/SubmitProduct";
 export default {
-    name: "MyProduct"
+    name: "MyProduct",
+    components: {SubmitProduct},
+    data() {
+        return {
+
+            submitProductDialogVisible: false
+        }
+    },
+    methods: {
+        closeSubmitProductDialog() {
+            this.submitProductDialogVisible = false
+        }
+    }
 }
 </script>
 
